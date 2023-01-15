485440069a574a06d55fb2a2a5584b22ba3417ef
Marta Litvinchuk <martalitvinchuk90@gmail.com>
2023-01-15 21:15:42 +0200

    appBar

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,18 +1,6 @@
-import { Header, NavItem } from './AppBar.styled';
-
-const navItem = [
-  { href: '', text: 'Home' },
-  { href: 'movies', text: 'Movies' },
-];
 
 export const AppBar = () => {
   return (
-    <Header>
-      {navItem.map(({ href, text }) => (
-        <NavItem to={href} key={text}>
-          {text}
-        </NavItem>
-      ))}
-    </Header>
+    <h1>Trending today</h1>
   );
 };
