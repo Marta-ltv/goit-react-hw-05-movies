b9ae8337aa47065dfaed7fbf45de998057ed74fe
Marta Litvinchuk <martalitvinchuk90@gmail.com>
2023-01-15 21:43:08 +0200

    update package.json

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,6 +1,16 @@
 
+// const navItem = [
+//   { href: '', text: 'Home' },
+//   { href: 'movies', text: 'Movies' },
+// ];
+
+
 export const AppBar = () => {
   return (
     <h1>Trending today</h1>
+
+    // <header>
+    //     <navItem.map
+    // </header>
   );
 };
