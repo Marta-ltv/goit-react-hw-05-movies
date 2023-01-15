e9ba149234a87fd0dad1dde7c985321e1b318269
Marta Litvinchuk <martalitvinchuk90@gmail.com>
2023-01-15 21:05:39 +0200

    add layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,16 +1,9 @@
-
-import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
-
 import { AppBar } from '../AppBar/AppBar';
 
 const Layout = () => {
   return (
     <main>
-      <AppBar />
-      <Suspense>
-        <Outlet />
-      </Suspense>
+      <AppBar /> 
     </main>
   );
 };
--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,38 +1,38 @@
-// const API_KEY = '963fd2e4c19fe891a746da17d3ea5ed3';
-// const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = '963fd2e4c19fe891a746da17d3ea5ed3';
+const BASE_URL = 'https://api.themoviedb.org/3';
 
-// async function fetchTrendingMovies() {
-//   const response = await fetch(`${BASE_URL}/trending/movie/day?api_key=${API_KEY}`);
-//     return await response.json();
-//   }
-// const apiDayMovies = { fetchTrendingMovies };
-// export default apiDayMovies;
+async function fetchTrendingMovies() {
+  const response = await fetch(`${BASE_URL}/trending/movie/day?api_key=${API_KEY}`);
+    return await response.json();
+  }
+const apiDayMovies = { fetchTrendingMovies };
+export default apiDayMovies;
 
-// export const searchMovie = async search => {
-//   const response = await fetch(
-//     `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${search}`
-//   );
-//   return await response.json();
-// };
+export const searchMovie = async search => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${search}`
+  );
+  return await response.json();
+};
    
-// export const fetchMovie = async movieId => {
-//   const response = await fetch(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`);
-//   return await response.json();
-// };
+export const fetchMovie = async movieId => {
+  const response = await fetch(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`);
+  return await response.json();
+};
 
-// export const castMovie = async movieId => {
-//   const response = await fetch(
-//     `${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}`
-//   );
-//   return await response.json();
-// };
+export const castMovie = async movieId => {
+  const response = await fetch(
+    `${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}`
+  );
+  return await response.json();
+};
 
-// export const reviewsMovie = async movieId => {
-//   const response = await fetch(
-//     `${BASE_URL}/movie/${movieId}/reviews?api_key=${API_KEY}`
-//   );
-//   return await response.json();
-// };
+export const reviewsMovie = async movieId => {
+  const response = await fetch(
+    `${BASE_URL}/movie/${movieId}/reviews?api_key=${API_KEY}`
+  );
+  return await response.json();
+};
 
 
 
